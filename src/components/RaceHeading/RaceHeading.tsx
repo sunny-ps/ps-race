7f449caa783a82096eddb875055258cb23a9d01a
sunny-ps <saneyar.khazin@publicissapient.com>
2023-01-16 09:55:17 +1100

    Revert final year edits

--- src/components/RaceHeading/RaceHeading.tsx
+++ src/components/RaceHeading/RaceHeading.tsx
@@ -54,8 +54,8 @@ const RaceHeading: FC<IRaceHeadingProps> = ({ dbData }) => {
     <div className="max-w-screen z-30">
       <div className="flex flex-col justify-center items-center px-7 pt-3 pb-3">
         <div className="flex justify-between text-white text-3xl mb-1 w-full">
-          {/*<p>{`Quarter ${getQuarter(currDate)} ${getYear(currDate)}`}</p>*/}
-          <p>{`Full Year ${getYear(currDate)}`}</p>
+          <p>{`Quarter ${getQuarter(currDate)} ${getYear(currDate)}`}</p>
+          {/* <p>{`Full Year ${getYear(currDate)}`}</p> */}
           {/*
           <p>{`${weeksRemaining} week${
             weeksRemaining > 1 ? "s" : ""
@@ -70,14 +70,14 @@ const RaceHeading: FC<IRaceHeadingProps> = ({ dbData }) => {
             />
           </div>
           */}
+          {/*
           <p>{`Full Year Target ${totalWorkSold().toFixed(
             1
           )}m / ${totalRevenueGuide().toFixed(1)}m`}</p>
-          {/*
-            <p>{`Quarter Target ${totalWorkSold().toFixed(
-            1
-            )}m / ${totalRevenueGuide().toFixed(1)}m`}</p>
           */}
+          <p>{`Quarter Target ${totalWorkSold().toFixed(
+            1
+          )}m / ${totalRevenueGuide().toFixed(1)}m`}</p>
         </div>
         <div className="w-[60%] h-full relative px-6">
           <img src="/Title.gif" alt="heading" />
